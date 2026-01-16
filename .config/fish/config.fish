d4e94ccd04c31668b6d35cce791e15194fd41a2b
Firdaus Hakimi <hakimifirdaus944@gmail.com>
2026-01-16 16:30:03 +0800

    another update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,18 +1,22 @@
 # pip token, tg bot token, apple music, etc
-if test -f credentials.fish
-    source credentials.fish
+if test -f $HOME/.config/fish/credentials.fish
+    source $HOME/.config/fish/credentials.fish
 end
+
+fish_add_path -Pp $HOME/.cargo/bin $HOME/bin $HOME/.local/bin
 
 set -U fish_greeting ""
 set -gx UV_PYTHON 3.14t
+set -gx -- EDITOR zed --wait
+set -gx GIT_PAGER delta
 
 ###########
 # ALIASES #
 ###########
 
 alias cat 'bat --style=changes,header,rule,numbers,snip'
-alias df 'duf'
-alias du 'dust'
+alias df duf
+alias du dust
 # alias ls 'lsd --group-directories-first' # preferred listing
 # alias la 'lsd -A --group-directories-first' # all files and dirs
 # alias ll 'lsd -l --blocks "permission,user,size,date,name" --date +"%e %b %I:%M %p" --group-directories-first --size short' # long format
@@ -20,7 +24,6 @@ alias du 'dust'
 # alias lt 'lsd --group-directories-first --tree' # tree listing
 # alias l. 'lsd -A --group-directories-first | grep "^\."' # show only dotfiles
 alias clear "printf '\e[H\e[3J'" # because what the fuck kitty
-alias tree 'br'
 
 #################
 # ABBREVIATIONS #
